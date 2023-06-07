70adf8dfc2dc599cdca48fce757820160eeef423
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-07 16:50:25 +0200

    Podaci

---
+++ data/test-podaci.js
@@ -0,0 +1,9 @@
+import Rad from '../models/rad'
+
+export const RADOVI = [
+  new Rad(0, "Ana", "React Native", "D"),
+  new Rad(1, "Ivan", "React", "D"),
+  new Rad(2, "Matea", "Baze podataka", "Z"),
+  new Rad(3, "Petar", "Umjetna inteligencija", "D"),
+  new Rad(4, "Iva", "Strojno učenje", "Z"),
+]
