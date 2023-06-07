ce12f378c80832237b24984521f1643c79611249
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-07 16:50:52 +0200

    Model za navigaciju

---
+++ models/rad.js
@@ -0,0 +1,10 @@
+class Rad {
+    constructor(id, student, naslov, vrsta){
+      this.id = id
+      this.student = student;
+      this.naslov = naslov;
+      this.vrsta = vrsta
+    }
+  }
+  
+  export default Rad
